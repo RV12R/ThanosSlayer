c27ccdcddc3699db46ecfcf449721303ba15660b
Rahul AV <>
2022-10-10 18:05:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,185 @@
+import { ethers } from 'ethers';
+import LoadingIndicator from './Components/LoadingIndicator';
+import Arena from './Components/Arena';
+import myEpicGame from './utils/MyEpicGame.json';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import SelectCharacter from './Components/SelectCharacter';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = 'itsrahulAV';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // State
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    
+        const checkNetwork = async () => {
+      try { 
+        if (window.ethereum.networkVersion !== '4') {
+          alert("Please connect to Rinkeby!")
+        }
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    setIsLoading(true);
+    checkIfWalletIsConnected();
+    
+  }, []);
+  
+  useEffect(() => {
+  /*
+   * The function we will call that interacts with out smart contract
+   */
+  const fetchNFTMetadata = async () => {
+    console.log('Checking for Character NFT on address:', currentAccount);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicGame.abi, signer);
+
+    const txn = await gameContract.checkIfUserHasNFT();
+    if (txn.name) {
+      console.log('User has character NFT');
+      setCharacterNFT(transformCharacterData(txn));
+    } else {
+      console.log('No character NFT found');
+    }
+    setIsLoading(false);
+  };
+
+  /*
+   * We only want to run this, if we have a connected wallet
+   */
+  if (currentAccount) {
+    console.log('CurrentAccount:', currentAccount);
+    fetchNFTMetadata();
+  }
+}, [currentAccount]);
+
+      // Render Methods
+  const renderContent = () => {
+    
+  /*
+   * Scenario #1
+   */ 
+    
+    if (isLoading) {
+    return <LoadingIndicator />;
+  }
+    
+  if (!currentAccount) {
+    return (
+      <div className="connect-wallet-container">
+        <img
+          src="https://64.media.tumblr.com/cafd4113961fb1143f66d972f8790461/c45c7c6945342a8c-fa/s540x810/3279db1df835601fbac2f99b483039449faa549e.gifv"
+          alt="RoKi Gif"
+        />
+        <button className="cta-button connect-wallet-button" onClick= {connectWalletAction}>
+    {currentAccount
+       ? `Wallet ${currentAccount.substring(0, 6)}...${currentAccount.substring( currentAccount.length - 4)} is connected`
+    : "Connect Wallet To Get Started"} 
+        </button>
+      </div>
+    );
+    /*
+     * Scenario #2
+     */
+  }  else if (currentAccount && !characterNFT) {
+    return <SelectCharacter setCharacterNFT={setCharacterNFT} />;	
+	/*
+	* If there is a connected wallet and characterNFT, it's time to battle!
+	*/
+  } else if (currentAccount && characterNFT) {
+    return <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
+  }
+  };
+  
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        setIsLoading(false);
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Thanos Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Avengverse!</p>
+          {renderContent()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xF3acEeED4C87DB3E19f5ED5561c2c604eaA64086';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
